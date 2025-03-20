5371d7345a326c50d3ef6b522a0537e4b0316b25
Leticia Alves de Pontes <50830736+leticia-hub@users.noreply.github.com>
2025-03-19 21:52:11 -0300

    add: adiciona decodificador 3x8

--- VHDL/CONTADOR4BITS_TESTBENCH/TB_CONTADOR4BITS.vhd
+++ VHDL/CONTADOR4BITS_TESTBENCH/TB_CONTADOR4BITS.vhd
@@ -6,54 +6,49 @@ ENTITY TB_CONTADOR4BITS IS
 END TB_CONTADOR4BITS;
 
 ARCHITECTURE TB OF TB_CONTADOR4BITS IS
-    COMPONENT CONTADOR4BITS IS
-        PORT(
-            CLK, RST, LOAD: IN STD_LOGIC;
-            DIN: IN STD_LOGIC_VECTOR(4 DOWNTO 0);
-            S: OUT STD_LOGIC_VECTOR(4 DOWNTO 0)
-        );
-    END COMPONENT;
-
-    SIGNAL CLK, RST, LOAD: STD_LOGIC;
-    SIGNAL DIN, S: STD_LOGIC_VECTOR(4 DOWNTO 0);
-    
+	COMPONENT CONTADOR4BITS IS
+	PORT(
+		CLK, RST, LOAD: IN STD_LOGIC;
+		DIN: IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+		S: OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
+	);
+   END COMPONENT;
+   
+   SIGNAL CLK, RST, LOAD, PAUSE: STD_LOGIC;
+   SIGNAL DIN, S: STD_LOGIC_VECTOR(3 DOWNTO 0);
 BEGIN
-    DUT: CONTADOR4BITS
-    PORT MAP (CLK  => CLK,
-              RST  => RST,
-              LOAD => LOAD,
-              DIN  => DIN,
-              S    => S);
-
-    CLK_PROCESS: PROCESS
-    BEGIN
-        WHILE TRUE LOOP
-            CLK <= '0';
-            WAIT FOR 5 NS;
-            CLK <= '1';
-            WAIT FOR 5 NS;
-        END LOOP;
-    END PROCESS;
-
-    SIMULATION: PROCESS
-    BEGIN
-        RST  <= '1';
-        LOAD <= '0';
-        DIN  <= (OTHERS => '0');
-
-        WAIT FOR 20 NS;
-        RST <= '0';
-
-        WAIT FOR 100 NS;
-
-        LOAD <= '1';
-        DIN  <= "00101";
-        WAIT FOR 10 NS;
-        LOAD <= '0';
-
-        WAIT FOR 100 NS;
-
-        WAIT;
-    END PROCESS;
-
+	DUT: CONTADOR4BITS
+	PORT MAP (CLK  => CLK,
+             RST  => RST,
+             LOAD => LOAD,
+             DIN  => DIN,
+             S    => S);
+	
+	CLK <= NOT CLK AFTER 50 NS WHEN PAUSE = '0' ELSE '0';
+	
+   STIMULI: PROCESS
+   BEGIN
+		PAUSE <= '0';
+		
+      RST  <= '0';
+      LOAD <= '0';
+      DIN  <= (OTHERS => '0');
+		WAIT FOR 100 NS;
+		
+		RST <= '1';
+		WAIT FOR 100 NS;
+		
+		RST <= '0';
+		WAIT FOR 2000 NS;
+     
+		DIN  <= "0101";
+		LOAD <= '1';
+		WAIT FOR 100 NS;
+      
+		LOAD <= '0';
+      WAIT FOR 2000 NS;
+		
+		PAUSE <= '1';
+		WAIT;
+   END PROCESS;	 
 END TB;
